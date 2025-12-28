e88254cce77c31bd131ec5f7cea99fb117eee65f
abhishekkr523 <7033abhishek@gmail.com>
2025-12-28 12:56:18 +0530

    bb

--- config/constants.php
+++ config/constants.php
@@ -1,8 +1,9 @@
 <?php
-define('BASE_URL', '');
+define('BASE_URL', '/trimeTime');
 ?>
 
 <?php
+// jkkkk
 // define('BASE_URL', 
 //     (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/trimeTime'
 // );
